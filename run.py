13e9a8946cffae2b793e30307fa0a0f156ae1e05
chloekeenan <chloe.keenan2001@gmail.com>
2023-04-19 07:24:49 +0000

    add pyfiglet import

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+ # import pyfiglet module
+# import pyfiglet module
+import pyfiglet
+
+result = pyfiglet.figlet_format("Weather Today", font = "bulbhead" )
+print(result)
+
+
 import requests
 api_key = '5cd00a3248c03a1e995276d733942885'
 
