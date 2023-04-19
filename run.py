520764e6dabecf3873fd89bcd95a1f5d33d6550b
chloekeenan <chloe.keenan2001@gmail.com>
2023-04-19 06:42:32 +0000

    add user input

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+api_key = '5cd00a3248c03a1e995276d733942885'
+
+user_input = input("Choose City:")
+
+print(user_input)
