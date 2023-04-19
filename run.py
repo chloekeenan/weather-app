b76bdd28824633f1957e4eef23ff5feb08b2ffdc
chloekeenan <chloe.keenan2001@gmail.com>
2023-04-19 06:50:43 +0000

    add weather data

--- run.py
+++ run.py
@@ -8,4 +8,11 @@ user_input = input("Choose City:")
 
 weather_data = requests.get (f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
 
-print(weather_data.status_code)
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºC")
