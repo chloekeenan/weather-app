dca9c4a64fa9d96c247c21798f8a1ccbb30627d5
chloekeenan <chloe.keenan2001@gmail.com>
2023-04-20 08:56:53 +0000

    fix blank spaces

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-
 import pyfiglet
 import requests
 api_key = '5cd00a3248c03a1e995276d733942885'
